a9f4bac4c0b05346cb7018bd3c4714ea2c6eb294
diego <ing.diegomahecha@gmail.com>
2022-08-02 16:52:53 -0500

    Revert "Update Main.java"
    This reverts commit dc849e6758f153383225690bae5cff525ce88984.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,11 @@
 package app;
 
-import login.LoginView;
-
 public class Main {
 
 	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
-		
-		LoginView ventanaP = new LoginView();
-		ventanaP.setVisible(true);
 		
 	} 
 		
