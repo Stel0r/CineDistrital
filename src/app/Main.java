dc849e6758f153383225690bae5cff525ce88984
diego <ing.diegomahecha@gmail.com>
2022-08-02 16:51:59 -0500

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,16 @@
 package app;
 
+import login.LoginView;
+
 public class Main {
 
 	
 	public static void main(String[] args) {
 		
 		System.out.println("hello");
+		
+		LoginView ventanaP = new LoginView();
+		ventanaP.setVisible(true);
 		
 	} 
 		
