d46af6bf694e45ce4d481ba273972b376c3721df
Stel0r <65517739+Stel0r@users.noreply.github.com>
2022-08-04 10:12:47 -0500

    Login Interface

--- src/interfaz/login/LoginView.java
+++ src/interfaz/login/LoginView.java
@@ -5,10 +5,31 @@ import javax.swing.*;
 
 public class LoginView extends JFrame {
 	
+	private JTextField userInput;
+	private JPasswordField passInput;
+	
 	public LoginView() {
-		this.setSize(1720, 820);
-		this.setVisible(true);
+		
+		
+		this.setSize(1000, 628);
 		this.getContentPane().setBackground(new Color(53, 53, 53));
+		this.setLayout(null);
+		
+		
+		
+		userInput = new JTextField();
+		userInput.setSize(300, 20);
+		userInput.setLocation((this.getSize().width/2)-userInput.getSize().width/2, 250);
+		this.add(userInput);
+		
+		passInput = new JPasswordField();
+		passInput.setSize(300, 20);
+		passInput.setLocation((this.getSize().width/2)-passInput.getSize().width/2, 350);
+		this.add(passInput);
+		
+		this.setVisible(true);
+		
+		
 	}
 
 }
