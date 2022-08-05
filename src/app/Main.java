7756415d2a422a5b21a514291d0e43eee7fcb725
Andres <72152489+AndresUD27@users.noreply.github.com>
2022-08-04 21:50:56 -0500

    Update Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,13 +3,14 @@ package app;
 import javax.swing.JFrame;
 
 import interfaz.login.LoginView;
+import interfaz.login.PeliculasView;
 
 public class Main {
 
 	
 	public static void main(String[] args) {
 		
-		JFrame ventana = new LoginView();
+		JFrame ventana = new PeliculasView();
 		
 	} 
 		
